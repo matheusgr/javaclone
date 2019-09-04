91a8ab181b1ddbd70b78d5cdfa9b5b2b0208037f
matheusgr <matheusgr@gmail.com>
2019-09-04 09:16:28 -0300

    Do a NJ and print tree result.

---
+++ javaclone/print_tree.py
@@ -0,0 +1,32 @@
+import sys
+
+
+from skbio import DistanceMatrix
+from skbio.tree import nj
+
+
+distance_matrix = {}
+codes_set = set()
+
+fname = sys.argv[1]
+
+for line in open(fname):
+    code1, code2, distance = line[0:41].strip(), line[41:81].strip(), 1 - float(line[81:].strip())
+    distance_vector = distance_matrix.get(code1, {})
+    distance_vector[code2] = distance
+    distance_matrix[code1] = distance_vector
+    distance_vector = distance_matrix.get(code2, {})
+    distance_vector[code1] = distance
+    distance_matrix[code2] = distance_vector
+    codes_set.add(code1)
+    codes_set.add(code2)
+    distance_matrix[code1][code1] = 0.0
+    distance_matrix[code2][code2] = 0.0
+
+distance_function = lambda x, y: distance_matrix[x][y]
+label_function = lambda x: x.replace(' ', '')
+
+dm = DistanceMatrix.from_iterable(codes_set, distance_function, label_function)
+
+tree = nj(dm, True)
+print(tree.ascii_art())
