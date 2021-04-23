409667a5354353cf473f9df45913c596a2363a26
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2021-04-23 16:01:32 -0300

    Removed unused load_iris import.

--- nj/main.py
+++ nj/main.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram
-from sklearn.datasets import load_iris
 from sklearn.cluster import AgglomerativeClustering
 
 import sys
