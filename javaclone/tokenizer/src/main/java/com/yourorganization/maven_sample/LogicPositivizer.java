eeaf2c7ae369959ffe8f5e235a1c17ecd0fb04f2
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2022-06-14 16:59:07 -0300

    Allow tokenizer to parse java zip files.

--- javaclone/tokenizer/src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ javaclone/tokenizer/src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -1,66 +1,51 @@
 package com.yourorganization.maven_sample;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.util.List;
 import java.util.Optional;
 
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.Problem;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.visitor.GenericVisitorWithDefaults;
-import com.github.javaparser.utils.SourceRoot;
 
 /**
  * Some code that uses JavaParser.
  */
 public class LogicPositivizer {
-    public static void main(String[] args) {
-        
-        SourceRoot sourceRoot = new SourceRoot(FileSystems.getDefault().getPath(args[0]));
-        List<ParseResult<CompilationUnit>> results;
-        boolean hasError = false;
 
-        try {
-            results = sourceRoot.tryToParse();
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-            System.exit(1);
-            return;
-        }
-        for (ParseResult<CompilationUnit> result : results) {
-            if (!result.isSuccessful()) {
-                for (Problem problem : result.getProblems()) {
-                    hasError = true;
-                    System.err.println(problem.toString());
-                }
-                continue;
+    public static boolean parse(ParseResult<CompilationUnit> result, String unitName) {
+        if (!result.isSuccessful()) {
+            for (Problem problem : result.getProblems()) {
+                System.err.println(problem.toString());
             }
-            Optional<CompilationUnit> parseResult = result.getResult();
-            if (parseResult.isPresent()) {
-                CompilationUnit cu = parseResult.get();
-                if (cu != null) {
-                    cu.accept(new GenericVisitorWithDefaults<Void, Void>() {
-                        @Override
-                        public Void defaultAction(Node n, Void arg) {
-                            recurse(n, 0, "ROOT");
-                            return super.defaultAction(n, arg);
-                        }
-                    }, null);
+            return false;
+        }
+        Optional<CompilationUnit> parseResult = result.getResult();
+        if (parseResult.isPresent()) {
+            CompilationUnit cu = parseResult.get();
+            for (ImportDeclaration importDeclaration : cu.getImports()) {
+                if (importDeclaration.getName().asString().contains("junit")) {
+                    return true;
                 }
             }
+            String pName = cu.getPrimaryTypeName().orElse(unitName);
+            cu.accept(new GenericVisitorWithDefaults<Void, Void>() {
+                @Override
+                public Void defaultAction(Node n, Void arg) {
+                    recurse(pName, n, 0, "ROOT");
+                    return super.defaultAction(n, arg);
+                }
+            }, null);
         }
-        if (hasError) {
-            System.exit(2);
-        }
+        return true;
     }
 
-    public static void recurse(Node n, int level, String parent) {
-        String currentStr = n.getClass().toString();
-        System.out.println(level + " " + currentStr + " " + parent);
+    public static void recurse(String pName, Node n, int level, String parent) {
+        String currentStr = n.getClass().getName();
+        System.out.println(pName + " " + level + " " + currentStr + " " + parent);
         for (Node nChild : n.getChildNodes()) {
-            recurse(nChild, level + 1, currentStr);
+            recurse(pName, nChild, level + 1, currentStr);
         }
     }
 }
---
+++ javaclone/tokenizer/src/main/java/com/yourorganization/maven_sample/Main.java
@@ -0,0 +1,61 @@
+package com.yourorganization.maven_sample;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.List;
+
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.utils.SourceRoot;
+import com.github.javaparser.utils.Pair;
+import com.github.javaparser.utils.SourceZip;
+
+
+public class Main {
+
+    public static void parseDir(String dirname) {
+        SourceRoot sourceRoot = new SourceRoot(FileSystems.getDefault().getPath(dirname));
+        List<ParseResult<CompilationUnit>> results;
+
+        try {
+            results = sourceRoot.tryToParse();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            System.exit(1);
+            return;
+        }
+        boolean hasError = false;
+        for (ParseResult<CompilationUnit> result: results) {
+            hasError |= LogicPositivizer.parse(result, null);
+        }
+        if (hasError) {
+            System.exit(2);
+        }
+    }
+
+    public static void parseZip(String zipFile) {
+        SourceZip sourceZip = new SourceZip(FileSystems.getDefault().getPath(zipFile));
+        List<Pair<Path, ParseResult<CompilationUnit>>> results;
+        try {
+            results = sourceZip.parse();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            System.exit(1);
+            return;
+        }
+        for (Pair<Path, ParseResult<CompilationUnit>> pair : results) {
+            LogicPositivizer.parse(pair.b, pair.a.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        if (args[0].toLowerCase().endsWith(".zip")) {
+            parseZip(args[0]);
+        } else {
+            parseDir(args[0]);
+        }
+    }
+
+}
